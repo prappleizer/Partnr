3a6e2cb280cb0db4c7f3795a76ff70f3ad43a98c
prappleizer <imad.pasha@yale.edu>
2023-05-23 15:21:12 -0400

    print statements

--- pages/02-💖_Matchr.py
+++ pages/02-💖_Matchr.py
@@ -231,8 +231,12 @@ with tab3:
             #comf = st_star_rating('Comfort',5,3,25)
             comf= st.slider('comfort',1,5,3,1)
         submit_extra = st.form_submit_button('Submit')
+        st.write(query.name)
+        st.write(query.id)
         if submit_extra:
             #print('test')
+            st.write(query.name)
+            st.write(query.id)
             table.update(query.id,{f'{User}-rating':rating,f'{User}-difficulty':diff,f'{User}-comfort':comf,'tried':'Yes'})
 
     reroll = st.button('Roll a new Option')
